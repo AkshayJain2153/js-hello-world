fa97a0266eb691326a89c19d013ec1975945b6a2
Akshay <122316281+AkshayJain2153@users.noreply.github.com>
2023-12-19 18:38:22 -0500

    <First changese>

--- hello.js
+++ hello.js
@@ -4,19 +4,42 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log('Welcome to LaunchCode great workplace');
+// console.log('Quoth the Raven "Nevermore"');
+
+// console.log("Hello, World " + "and welcome!");
+// console.log("Hello, World");
+// console.log(5 + 6);
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
+   Ans:- Did console.log("Hello, World");
+
    2. Figure out what the parentheses do. Will the code work without them?
+   Ans:- No
+
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+   Ans:- We can use either but both qutes have to be same i.e.  both "" or ''.
+   
    4. Remove the semi-colon, ;.
+   Ans:- It will work.
+
    5. Print a number. (Bonus: Print two numbers added together).
+   Ans:- console.log(5); and console.log(5+6);
+
    6. Print multiple messages one after the other.
+   Ans:- console.log("Hello, World");
+         console.log(5+6);
+
    7. Print two messages on the same line.
+   Ans:- console.log("Hello, World " + "and welcome!");   
+
+
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+   Ans:- console.log('Quoth the Raven "Nevermore"');
+
    9. Other. You choose!
 */
